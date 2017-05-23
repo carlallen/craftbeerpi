71b8480c35019cc7ee9b66077c6d889426a5f0ac
Carl Allen <carl@allenofmn.com>
2017-05-23 17:30:54 -0500

    PID to calculate chamber set temp

--- brewapp/base/fermenter.py
+++ brewapp/base/fermenter.py
@@ -9,16 +9,18 @@ from brewapp import app, socketio, manager
 import datetime
 from brewapp.base.util import *
 from brewapp.base.actor import *
+from automatic.pid_arduino import *
 
 
 app.cbp['CURRENT_TASK'] = {}
 app.cbp['FERMENTERS'] = {}
+app.cbp['FERMENTER_CHAMBER_TARGETS'] = {}
 
 
 @brewinit()
 def load():
     for s in Fermenter.query.all():
-        app.cbp['FERMENTERS'][s.id] = to_dict(s)
+        app.cbp['FERMENTERS'][s.id] = to_dict(s, include_methods=["chamber_target_temp"])
     for s in FermenterStep.query.filter_by(state='A').all():
         app.cbp['CURRENT_TASK'][s.fermenter_id] = to_dict(s)
         if s.state == 'A' and s.timer_start is not None:
@@ -42,14 +44,14 @@ def post_patch(result, **kw):
 
 def reload_fermenter(id):
     f = Fermenter.query.get(id)
-    d = to_dict(f, deep={'steps': []})
+    d = to_dict(f, deep={'steps': []}, include_methods=["chamber_target_temp"])
     app.cbp['FERMENTERS'][f.id] = d
     socketio.emit('fermenter_update', d, namespace='/brew')
 
 
 
 
-manager.create_api(Fermenter, methods=['GET', 'POST', 'PUT', 'DELETE'],  results_per_page=None, postprocessors={ 'PUT_SINGLE': [post_patch], 'POST': [post_post]})
+manager.create_api(Fermenter, methods=['GET', 'POST', 'PUT', 'DELETE'],  results_per_page=None, postprocessors={ 'PUT_SINGLE': [post_patch], 'POST': [post_post]}, include_methods=["chamber_target_temp"])
 manager.create_api(FermenterStep, methods=['GET', 'POST', 'PUT', 'DELETE'], results_per_page=None, postprocessors={'PUT_SINGLE': [post_patch]})
 
 @app.route('/api/fermenter/step/order', methods=['POST'])
@@ -130,18 +132,29 @@ def fermenter_state():
     return json.dumps(app.brewapp_automatic_state)
 
 def hystresis(id):
+    pid = PIDArduino(1, 2.0, 0.02, 5, -15.0, 15.0)
     while app.brewapp_automatic_state["F" + id]:
 
         fermenter = app.cbp['FERMENTERS'][int(id)]
-
 
         if type(fermenter["sensorid"]) is not int:
             socketio.emit('message', {"headline": "NO_TERMOMETER", "message": "NO_THERMOMETER_DEFINED"}, namespace='/brew')
             break
 
         temp = app.brewapp_thermometer_last[fermenter["sensorid"]]
-
         target_temp = fermenter["target_temp"]
+
+        if type(fermenter["chambersensorid"]) is not int:
+            chamber_temp = temp
+            chamber_target_temp = target_temp
+        else:
+            chamber_target_temp = target_temp + pid.calc(temp, target_temp)
+            chamber_temp = app.brewapp_thermometer_last[fermenter["chambersensorid"]]
+
+        fermenter["chamber_target_temp"] = chamber_target_temp
+        app.cbp['FERMENTER_CHAMBER_TARGETS'][int(id)] = chamber_target_temp
+        socketio.emit('fermenter_update', fermenter, namespace='/brew')
+
         heater_min = fermenter["heateroffset_min"]
         heater_max = fermenter["heateroffset_max"]
 
@@ -153,17 +166,17 @@ def hystresis(id):
 
 
         if heater_id is not None:
-            if temp + heater_min < target_temp:
+            if temp + heater_min < target_temp and chamber_temp + heater_min < chamber_target_temp:
                 switchOn(fermenter["heaterid"])
 
-            if temp + heater_max > target_temp:
+            if temp + heater_max > target_temp and chamber_temp + heater_max > chamber_target_temp:
                 switchOff(fermenter["heaterid"])
 
         if cooler_id is not None:
-            if temp > target_temp + cooler_min:
+            if temp > target_temp + cooler_min and chamber_temp > chamber_target_temp + cooler_min:
                 switchOn(fermenter["coolerid"])
 
-            if temp < target_temp + cooler_max:
+            if temp < target_temp + cooler_max and chamber_temp < chamber_target_temp + cooler_max:
                 switchOff(fermenter["coolerid"])
 
         socketio.sleep(1)
